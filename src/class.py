50e12a71fa5f9ff02ba95d16cd33249815f6bba2
DeckyAL <decky.aspandi.latif@gmail.com>
2017-04-03 21:57:52 +0200

    UPDATED

--- src/class.py
+++ src/class.py
@@ -3,20 +3,24 @@ Created on Apr 3, 2017
 
 @author: deckyal
 '''
-class calculator : 
-    def addition(x,y):
+class MyCalculator : 
+    
+    def addition(self,x,y):
         added = x + y;
         print(added);
         
-    def substraction(x,y):
+    def substraction(self, x,y):
         sub = x-y;
         print(sub)
         
-    def multiplication(x,y):
+    def multiplication(self, x,y):
         mult = x*y;
         print mult;
         
         
-    def division(x,y):
+    def division(self,x,y):
         div = x,y;
         print div;
+
+c = MyCalculator();
+c.addition(5,8)
